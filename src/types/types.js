193981736b8c34b980c0ff4abb18e39538201b0e
LorenitaTL <lore7096@gmail.com>
2020-11-04 22:39:02 -0600

    Upload project

---
+++ src/types/types.js
@@ -0,0 +1,25 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventStartAddNew:'[event] Start add new',
+    eventSetActive: '[event] Set Active',
+    eventLogout:'[event] Logout event',
+    eventAddNew: '[event] Add new',
+    eventClearActiveEvent: '[event] Clear active event',
+    eventUpdated: '[event] Event updated',
+    eventDeleted: '[event] Event deleted',
+    eventLoaded: '[event] s loaded',
+
+    
+    authCheckingFinish: '[auth] Finish checking login state',
+    authStartLogin: '[auth] Start login',
+    authLogin:'[auth] Login',
+    authStartRegister:'[auth] Start register',
+    authStartTokenRenew:'[auth] Start token renew',
+    authLogout:'[auth] Logout',
+
+
+}
